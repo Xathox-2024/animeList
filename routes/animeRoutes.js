636420f7f5d67ce37c97f92eff69f3f1cd0775c2
tom <162759484+Xathox-2024@users.noreply.github.com>
2025-11-07 10:32:12 +0100

    Update animeRoutes.js

--- routes/animeRoutes.js
+++ routes/animeRoutes.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const { getAnimeList, listAnimesJson, createAnime } =
   require("../controllers/animes/animesControllers");
 
-const isAuthenticated = require("./Middlewares/authRoutes/isAuthenticated");
 const multerCloudinary = require("./Middlewares/authRoutes/multerCloudinary");
 const validateAnime = require("./Middlewares/validateAnime");
 
 router.get("/anime", getAnimeList);
 router.get("/api/anime", listAnimesJson);
 router.post("/anime",
-  isAuthenticated,
   multerCloudinary.single("image"),
   validateAnime,
   createAnime
